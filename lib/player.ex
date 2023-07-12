be3e1f45b8026b972f729f6bf6a3ddcfcadc088e
papilon087 <ademirgus@gmail.com>
2023-07-12 15:33:21 -0300

    feat: add function player and struct improvement

--- lib/player.ex
+++ lib/player.ex
@@ -1,15 +1,18 @@
 defmodule ExMon.Player do
-  @enforce_keys [:life, :name, :move_rnd, :move_avg, :move_heal]
+  # Variavel de modulo
+  @ require_keys  [:life, :move_avg, :move_heal, :move_rnd, :name]
+  @max_life 100
 
-  defstruct [:life, :name, :move_rnd, :move_avg, :move_heal]
+  @enforce_keys @require_keys
+  defstruct @require_keys
 
   def build(name, move_rnd, move_avg, move_heal) do
     %ExMon.Player{
-      name: name,
-      move_rnd: move_rnd,
+      life: @max_life,
       move_avg: move_avg,
       move_heal: move_heal,
-      life: 100
+      move_rnd: move_rnd,
+      name: name
     }
   end
 end
